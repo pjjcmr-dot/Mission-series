6bcfd00cde1c2798d5ba4a802cca9400ce3e6c68
박종장 <pjjcmr@gmail.com>
2026-02-04 17:01:28 +0900

    m4

--- sprint-4/src/App.jsx
+++ sprint-4/src/App.jsx
@@ -1,8 +1,9 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
-import thumbnail from "./assets/thumbnail.png";
-import likeIcon from "./assets/icons/like.svg";
+// import thumbnail from "./assets/tail.png";
+// import likeIcon from "./assets/icons/humbnlike.svg";
 import { BestProductList } from "./components/BestProductList";
+import { ProductList } from "./components/ProductList";
 
 function App() {
   return (
@@ -13,178 +14,17 @@ function App() {
         <h1 className="sr-only">중고마켓 - 판다마켓</h1>
 
         <section>
-          <h2 className="text-[#111827] font-blod text-[20px] leading-8">
+          <h2 className="text-[#111827] font-bold text-[20px] leading-8">
             베스트 상품
           </h2>
           <BestProductList />
         </section>
 
         <section className="mt-10">
-          <h2 className="text-[#111827] font-blod text-[20px] leading-8">
+          <h2 className="text-[#111827] font-bold text-[20px] leading-8">
             판매중인 상품
           </h2>
-          <ul className="grid gap-6 mt-4 grid-cols-5">
-            <li>
-              <img
-                width={221}
-                height={221}
-                src={thumbnail}
-                alt="thumbnail"
-                className="aspect-square rounded-2xl"
-              />
-              <div className="flex flex-col gap-1.5 mt-4 text-[#1F2937]">
-                <h3 className="text-[14px]">아이패드 미니 팝니다</h3>
-                <span className="font-bold">500,000원</span>
-                <div className="text-[12px] flex gap-1 text-[#4B5563]">
-                  <img src={likeIcon} alt="좋아요" /> 240
-                </div>
-              </div>
-            </li>
-            <li>
-              <img
-                width={221}
-                height={221}
-                src={thumbnail}
-                alt="thumbnail"
-                className="aspect-square rounded-2xl"
-              />
-              <div className="flex flex-col gap-1.5 mt-4 text-[#1F2937]">
-                <h3 className="text-[14px]">아이패드 미니 팝니다</h3>
-                <span className="font-bold">500,000원</span>
-                <div className="text-[12px] flex gap-1 text-[#4B5563]">
-                  <img src={likeIcon} alt="좋아요" /> 240
-                </div>
-              </div>
-            </li>
-            <li>
-              <img
-                width={221}
-                height={221}
-                src={thumbnail}
-                alt="thumbnail"
-                className="aspect-square rounded-2xl"
-              />
-              <div className="flex flex-col gap-1.5 mt-4 text-[#1F2937]">
-                <h3 className="text-[14px]">아이패드 미니 팝니다</h3>
-                <span className="font-bold">500,000원</span>
-                <div className="text-[12px] flex gap-1 text-[#4B5563]">
-                  <img src={likeIcon} alt="좋아요" /> 240
-                </div>
-              </div>
-            </li>
-            <li>
-              <img
-                width={221}
-                height={221}
-                src={thumbnail}
-                alt="thumbnail"
-                className="aspect-square rounded-2xl"
-              />
-              <div className="flex flex-col gap-1.5 mt-4 text-[#1F2937]">
-                <h3 className="text-[14px]">아이패드 미니 팝니다</h3>
-                <span className="font-bold">500,000원</span>
-                <div className="text-[12px] flex gap-1 text-[#4B5563]">
-                  <img src={likeIcon} alt="좋아요" /> 240
-                </div>
-              </div>
-            </li>
-            <li>
-              <img
-                width={221}
-                height={221}
-                src={thumbnail}
-                alt="thumbnail"
-                className="aspect-square rounded-2xl"
-              />
-              <div className="flex flex-col gap-1.5 mt-4 text-[#1F2937]">
-                <h3 className="text-[14px]">아이패드 미니 팝니다</h3>
-                <span className="font-bold">500,000원</span>
-                <div className="text-[12px] flex gap-1 text-[#4B5563]">
-                  <img src={likeIcon} alt="좋아요" /> 240
-                </div>
-              </div>
-            </li>
-            <li>
-              <img
-                width={221}
-                height={221}
-                src={thumbnail}
-                alt="thumbnail"
-                className="aspect-square rounded-2xl"
-              />
-              <div className="flex flex-col gap-1.5 mt-4 text-[#1F2937]">
-                <h3 className="text-[14px]">아이패드 미니 팝니다</h3>
-                <span className="font-bold">500,000원</span>
-                <div className="text-[12px] flex gap-1 text-[#4B5563]">
-                  <img src={likeIcon} alt="좋아요" /> 240
-                </div>
-              </div>
-            </li>
-            <li>
-              <img
-                width={221}
-                height={221}
-                src={thumbnail}
-                alt="thumbnail"
-                className="aspect-square rounded-2xl"
-              />
-              <div className="flex flex-col gap-1.5 mt-4 text-[#1F2937]">
-                <h3 className="text-[14px]">아이패드 미니 팝니다</h3>
-                <span className="font-bold">500,000원</span>
-                <div className="text-[12px] flex gap-1 text-[#4B5563]">
-                  <img src={likeIcon} alt="좋아요" /> 240
-                </div>
-              </div>
-            </li>
-            <li>
-              <img
-                width={221}
-                height={221}
-                src={thumbnail}
-                alt="thumbnail"
-                className="aspect-square rounded-2xl"
-              />
-              <div className="flex flex-col gap-1.5 mt-4 text-[#1F2937]">
-                <h3 className="text-[14px]">아이패드 미니 팝니다</h3>
-                <span className="font-bold">500,000원</span>
-                <div className="text-[12px] flex gap-1 text-[#4B5563]">
-                  <img src={likeIcon} alt="좋아요" /> 240
-                </div>
-              </div>
-            </li>
-            <li>
-              <img
-                width={221}
-                height={221}
-                src={thumbnail}
-                alt="thumbnail"
-                className="aspect-square rounded-2xl"
-              />
-              <div className="flex flex-col gap-1.5 mt-4 text-[#1F2937]">
-                <h3 className="text-[14px]">아이패드 미니 팝니다</h3>
-                <span className="font-bold">500,000원</span>
-                <div className="text-[12px] flex gap-1 text-[#4B5563]">
-                  <img src={likeIcon} alt="좋아요" /> 240
-                </div>
-              </div>
-            </li>
-            <li>
-              <img
-                width={221}
-                height={221}
-                src={thumbnail}
-                alt="thumbnail"
-                className="aspect-square rounded-2xl"
-              />
-              <div className="flex flex-col gap-1.5 mt-4 text-[#1F2937]">
-                <h3 className="text-[14px]">아이패드 미니 팝니다</h3>
-                <span className="font-bold">500,000원</span>
-                <div className="text-[12px] flex gap-1 text-[#4B5563]">
-                  <img src={likeIcon} alt="좋아요" /> 240
-                </div>
-              </div>
-            </li>
-          </ul>
+          <ProductList />
         </section>
       </main>
 
--- sprint-4/src/components/productList.jsx
+++ sprint-4/src/components/productList.jsx
@@ -12,8 +12,6 @@ import { useEffect, useState } from "react";
 //   }
 // ];
 
-//더미데이터로 우선 강의 설명하는 상호아임
-
 const dummyProducts = [
   {
     src: "https://picsum.photos/seed/1/300/300",
@@ -43,32 +41,74 @@ const dummyProducts = [
     title: "홈카페 필수 아이템",
     likeCount: 31,
   },
+  {
+    src: "https://picsum.photos/seed/5/300/300",
+    name: "디자인 탁상 시계",
+    price: "55,000",
+    title: "세련된 공간 완성 탁상시계",
+    likeCount: 19,
+  },
+  {
+    src: "https://picsum.photos/seed/6/300/300",
+    name: "포스터 액자 LED 스탠드",
+    price: "28,000",
+    title: "무드등 겸용 스탠드",
+    likeCount: 8,
+  },
+  {
+    src: "https://picsum.photos/seed/7/300/300",
+    name: "13~14인치 노트북 파우치",
+    price: "47,000",
+    title: "방수 기능 노트북 파우치",
+    likeCount: 22,
+  },
+  {
+    src: "https://picsum.photos/seed/8/300/300",
+    name: "일출부터 일몰까지 반려견",
+    price: "33,000",
+    title: "강아지 산책 일기",
+    likeCount: 41,
+  },
+  {
+    src: "https://picsum.photos/seed/9/300/300",
+    name: "무선 보온 머그컵",
+    price: "18,000",
+    title: "따뜻한 커피 유지",
+    likeCount: 36,
+  },
+  {
+    src: "https://picsum.photos/seed/10/300/300",
+    name: "캐리어 여행 가방",
+    price: "150,000",
+    title: "디자인 깔끔한 여행용 캐리어",
+    likeCount: 16,
+  },
 ];
 
-export function BestProductList() {
+export function ProductList() {
   const [products, setProducts] = useState(dummyProducts);
 
   // useEffect(() => {
-  //   const getBestProducts = async () => {
+  //   const getProducts = async () => {
   //     const res = await fetch(
-  //       "https://panda-market-api.vercel.app/products?page=1&pageSize=4&orderBy=favorite",
+  //       "https://panda-market-api.vercel.app/products?page=1&pageSize=10&orderBy=recent",
   //     );
   //     const data = await res.json();
   //     setProducts(data.list);
   //   };
-  //   getBestProducts();
+  //   getProducts();
   // }, []); // 의존성 배열이라고 해서 실행이 딱 한번만 된다.
 
   // console.log(products);
-  // 렌더링 하고 싶을때는 배열 매소드 사용한다.
+
   return (
-    <ul className="flex gap-6 mt-4">
+    <ul className="grid grid-cols-5 gap-6 mt-4">
       {products.map((product) => {
         return (
           <li>
             <img
-              width={282}
-              height={282}
+              width={221}
+              height={221}
               src={product.src}
               alt="thumbnail"
               className="aspect-square rounded-2xl"
@@ -83,55 +123,6 @@ export function BestProductList() {
           </li>
         );
       })}
-
-      {/* <li>
-        <img
-          width={282}
-          height={282}
-          src={thumbnail}
-          alt="thumbnail"
-          className="aspect-square rounded-2xl"
-        />
-        <div className="flex flex-col gap-1.5 mt-4 text-[#1F29274]">
-          <h3 className="text-[14px]">아이패드 미니 팝니다</h3>
-          <span className="font-bold">500,000원</span>
-          <div className="text-[12px] flex gap-1">
-            <img src={likeIcon} alt="좋아요" /> 240
-          </div>
-        </div>
-      </li>
-      <li>
-        <img
-          width={282}
-          height={282}
-          src={thumbnail}
-          alt="thumbnail"
-          className="aspect-square rounded-2xl"
-        />
-        <div className="flex flex-col gap-1.5 mt-4 text-[#1F29274]">
-          <h3 className="text-[14px]">아이패드 미니 팝니다</h3>
-          <span className="font-bold">500,000원</span>
-          <div className="text-[12px] flex gap-1">
-            <img src={likeIcon} alt="좋아요" /> 240
-          </div>
-        </div>
-      </li>
-      <li>
-        <img
-          width={282}
-          height={282}
-          src={thumbnail}
-          alt="thumbnail"
-          className="aspect-square rounded-2xl"
-        />
-        <div className="flex flex-col gap-1.5 mt-4 text-[#1F29274]">
-          <h3 className="text-[14px]">아이패드 미니 팝니다</h3>
-          <span className="font-bold">500,000원</span>
-          <div className="text-[12px] flex gap-1">
-            <img src={likeIcon} alt="좋아요" /> 240
-          </div>
-        </div>
-      </li> */}
     </ul>
   );
 }
